9bb5af040a9bb5fa71b72aaa432dc19d338e0a46
Felipe Sena <felipedepaulasena@hotmail.com>
2019-01-29 14:03:55 -0200

    Add new endpoints to client and social name

---
+++ src/main/java/com/fillexc/repository/SocialNameRepository.java
@@ -0,0 +1,7 @@
+package com.fillexc.repository;
+
+import com.fillexc.domain.SocialName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialNameRepository extends JpaRepository<SocialName, Integer> {
+}
--- src/main/java/com/fillexc/service/FillexcService.java
+++ src/main/java/com/fillexc/service/FillexcService.java
@@ -1,7 +1,10 @@
 package com.fillexc.service;
 
 import com.fillexc.domain.Client;
+import com.fillexc.domain.SocialName;
 import com.fillexc.repository.ClientRepository;
+import com.fillexc.repository.SocialNameRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class FillexcService {
 
@@ -16,10 +20,24 @@ public class FillexcService {
     private ClientRepository clientRepo;
 
     @Autowired
+    private SocialNameRepository socialNameRepo;
+
+    @Autowired
     private S3Service s3Service;
 
     public ResponseEntity<Client> addClient(Client client) {
         return new ResponseEntity(clientRepo.save(client), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity getClient(int id) {
+        Client client = clientRepo.getOne(id);
+
+        if (client == null) {
+            log.warn(String.format("Client with id %d not found", id));
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity(client, HttpStatus.OK);
     }
 
     public ResponseEntity getAllClients() {
@@ -29,5 +47,30 @@ public class FillexcService {
             return new ResponseEntity(clients, HttpStatus.NO_CONTENT);
 
         return new ResponseEntity(clients, HttpStatus.OK);
+    }
+
+    public ResponseEntity getAllSocialNames() {
+        List<SocialName> socialNames = socialNameRepo.findAll();
+
+        if (socialNames.size() == 0) {
+            return new ResponseEntity(socialNames, HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity(socialNames, HttpStatus.OK);
+    }
+
+    public ResponseEntity<SocialName> addSocialName(SocialName socialName) {
+        return new ResponseEntity(socialNameRepo.save(socialName), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity getSocialName(int id) {
+        SocialName socialName = socialNameRepo.getOne(id);
+
+        if (socialName == null) {
+            log.warn(String.format("Social name with id %d not found", id));
+            return ResponseEntity.notFound().build();
+        }
+
+        return new ResponseEntity(socialName, HttpStatus.OK);
     }
 }
